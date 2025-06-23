adb22d6d3371085986dfa9d07ba9dfa874e0800f
Denis0773593206 <carldenisonrard@gmail.com>
2025-06-23 13:30:14 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+let fontSizeDropdown = document. getElementById('font -size');
+let heading = document .getElementById('heading');
+
+fontSizeDropdown.addEventListener("change", function()){
+    console.log(fontSizeDropdown.valve)
+    let fontSize = fontSizeDropdown.value;
+}
+
+let background color Dropdown = document. getElementById('backgound color');
+let heading = document .getElementById('heading');
+
+background colorDropdown.addEventListener("change", function()){
+    console.log(backgroundColorDropdown.valve)
+    let background color = backgroundColorDropdown.value;
+}
